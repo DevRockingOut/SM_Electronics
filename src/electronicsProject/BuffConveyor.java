5b7feaa99e0aef1bc97c847638e055a75d4809fc
devrockingout <econt037@uottawa.ca>
2020-04-16 18:02:32 -0400

    updated BuffConveyor, removed BufferType

--- src/electronicsProject/BuffConveyor.java
+++ src/electronicsProject/BuffConveyor.java
@@ -7,31 +7,9 @@ class BuffConveyor {
 	protected int n;
     protected Part[] list;
     protected int capacity;
-    protected PartType conveyorPartType; // ?? [WTF_QUESTION]   I think we should store bufferType instead
-    protected BufferType type;
+    protected PartType conveyorPartType;
     protected static final BuffConveyor NO_BUFF_CONVEYOR = null;
-    protected static final BufferType BUFF_TYPE_NONE = null;
-    
-    // assigning integer values to each enum member
-    protected enum BufferType { 
-    	BA(0, "BA"), BB(1, "BB"), BC(2, "BC");
-    	
-		private final int value;
-		private final String svalue;
-
-	    private BufferType(int value, String svalue) {
-	        this.value = value;
-	        this.svalue = svalue;
-	    }
-
-	    public int getInt() {
-	        return value;
-	    }
-	    
-	    public String getString() {
-	    	return svalue;
-	    }
-	}
+   
     
     BuffConveyor(int capacity){
         this.capacity = capacity;
