c7e417b58801dd6c54b9d8a86f8ecf37f39143d7
devrockingout <econt037@uottawa.ca>
2020-04-27 06:24:14 -0400

    added comments to exp file

--- src/Experimentation.java
+++ src/Experimentation.java
@@ -5,7 +5,7 @@ import cern.jet.random.engine.RandomSeedGenerator;
 public class Experimentation {
 	
     static final int NUMRUNS = 20;
-    final static int numPallets = 72;
+    static int numPallets = 72;
 	final static int [] BatchSize = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     final static double HOUR = 60 * 60;
     final static double WEEK = 5 * 16 * HOUR;  // 5 day week, 16 hours/day
@@ -22,32 +22,53 @@ public class Experimentation {
 	    RandomSeedGenerator rsg = new RandomSeedGenerator();
 	     
 	    for(int i = 0; i < numPallets*BatchSize.length; i++) { sds[i] = new Seeds(rsg);}
-	    int count = 0;
+
 		//for(int i = 40; i <= numPallets; i++)
 		//{
-	  
+		    
+	    int pallet = 40;
+	    int batchSize = 0;
+	    int count = 0;
+	    // just increment the count always
+	    // start at 0
+	    // pallet at 40
+	    // batchsize at 0
+	    // start by increasing pallets up to 72
+	    // and then when you reach 72 you should be at the end of that column because no batch size
+	    // then reset pallets to 40
+	    // increment batchsize by 1
+	    // and whenever you change pallet+1  or  batchSize+1 
+	    // just increase the count
+	    // and so on
+	    // get it?yessss
+	    // lol im like did you just say yess so I shut up?nooooooooooooooooooooooooooo
+	    // ok XD
+	    // cuz fdjafndafd nvm lol
 	    
-			ElectronicsProject model = new ElectronicsProject(endTime, 40, 0, sds[count], false, 0);
+	    /*******************   The code below for printing pallets with NO batchSize   *************************/
+
+	   /* ElectronicsProject model = new ElectronicsProject(endTime, pallet, batchSize, sds[count], false, 0);
 		    model.setTimef(WARM_UP_PERIOD);
 		    model.runSimulation();
 		    //model.clearLostCost();
 		    
 		    model.setTimef(endTime);
-		    model.runSimulation();             
-		    lostCost_step2[40] = model.getLostCost()/NUM_WEEKS;
+		    model.runSimulation();    //  there we go my bad
+		    lostCost_step2[pallet] = model.getLostCost()/NUM_WEEKS;
 		    model.clearLostCost();
-		    count++;
+		    count++;*/
 		    
-		    model = new ElectronicsProject(endTime, 50, 0, sds[count], false, 0);
-		    model.setTimef(WARM_UP_PERIOD);
-		    model.runSimulation();
-		    //model.clearLostCost();
+		/*******************   The code below for printing pallets with batchSize   *************************/
 		    
-		    model.setTimef(endTime);
-		    model.runSimulation();             
-		    lostCost_step2[50] = model.getLostCost()/NUM_WEEKS;
-		    model.clearLostCost();
-		    count++;
+		    ElectronicsProject model1 = new ElectronicsProject(endTime, pallet, batchSize, sds[count], false, 0);
+			 model1.setTimef(WARM_UP_PERIOD);
+			 model1.runSimulation();
+		     //model1.clearLostCost(); 
+		     model1.setTimef(endTime);
+		     model1.runSimulation();             
+		     lostCost_step3AND4[pallet][batchSize-1] = model1.getLostCost()/NUM_WEEKS;
+		     model1.clearLostCost(); 
+		     count++;
 		    
 		//}
 		   // System.out.println("ADD_PALLET_NUMBER: " + addPallet);
